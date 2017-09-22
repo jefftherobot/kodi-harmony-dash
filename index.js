1181f9731d448530d9dac1d6d8dc0113bae151af
Jeff Mills <jeffy9@gmail.com>
2017-09-21 23:08:35 -0400

    ping kodi now

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const DashButton = require('dash-button');
 const harmony = require('harmonyhubjs-client');
 const kodi = require('kodi-ws');
+const exec = require('child_process').exec;
+
+
 const DASH_BUTTON_MAC_ADDRESS = '40:b4:cd:ba:25:f3';
 //const HARMONY_IP_ADDRESS = '192.168.0.242';
 const HARMONY_IP_ADDRESS = '192.168.0.118';
 const KODI_IP_ADDRESS = '192.168.0.128';
 
+
 let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
 let subscription = button.addListener(async () => {
-  tvOnOff();
+	playTvShow();
 });
 
 function playTvShow(){
+	//First turn kodi activity on and start pinging it
+	tvOnOff();
+	pingKodi(()=>{
+		console.log('====> Kodi is alive!!')
+		buildPlaylist()
+	});
+
+	
+}
+
+
+function buildPlaylist(){ console.log('build playlist')
+
 	kodi(KODI_IP_ADDRESS, 9090).then(function (connection) {
 		return connection.VideoLibrary.GetEpisodes({ 
 			'tvshowid': 48, //teen titans go! 
@@ -42,16 +59,17 @@ function playTvShow(){
 		})
 
 	}).catch(function(e) {
-	/* Handle errors */
-	if(e.stack) {
-		console.error(e.stack);
-	} else {
-		console.error(e);
-	}
-}).then(function() {
-	/* Finally exit this process */
-	//process.exit();
-});
+		/* Handle errors */
+		if(e.stack) {
+			console.error(e.stack);
+		} else {
+			console.error(e);
+		}
+	}).then(function() {
+		/* Finally exit this process */
+		//process.exit();
+	});
+
 }
 
 
@@ -70,12 +88,6 @@ function tvOnOff(){
 	                  var id = activity.id
 	                  harmonyClient.startActivity(id)
 	                  harmonyClient.end()
-	                  
-	                  setTimeout(function(){
-	                  	playTvShow();
-	                  },10000)
-
-
 	                  return true
 	                }
 	                return false
@@ -88,4 +100,32 @@ function tvOnOff(){
 	        }
 	      })
   	})
+}
+
+
+function pingKodi(cb){
+	new ping(KODI_IP_ADDRESS,(e)=>{
+		if(e){
+			return console.log('====> Fail');
+		}
+		return cb();
+  })
+}
+
+
+function ping (host,cb){
+	(function pinghost(host){exec('ping -c 1 -n '+host,(e,stdOut,stdErr)=>{
+		if(e) {
+			pinghost(host);
+			return cb(e);
+		}
+		
+		if(stdErr) {
+			pinghost(host);
+			return cb(stdErr)
+		}
+		
+		//pinghost(host);
+		return cb(null);
+	})})(host)
 }
