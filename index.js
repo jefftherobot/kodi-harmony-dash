c8482606c99cc2e48a2ae21f74b6f3bd14e33f09
Jeff Mills <jeff@jefftherobot.com>
2017-09-22 20:32:40 -0400

    get tvshowid

--- index.js
+++ index.js
@@ -2,61 +2,72 @@ const DashButton = require('dash-button');
 const harmony = require('harmonyhubjs-client');
 const kodi = require('kodi-ws');
 const exec = require('child_process').exec;
+const filter = require("lodash.filter");
 
 
 const DASH_BUTTON_MAC_ADDRESS = '40:b4:cd:ba:25:f3';
 //const HARMONY_IP_ADDRESS = '192.168.0.242';
 const HARMONY_IP_ADDRESS = '192.168.0.118';
-const KODI_IP_ADDRESS = '192.168.0.128';
+const KODI_IP_ADDRESS = '192.168.0.205';
+//const KODI_IP_ADDRESS = '192.168.0.128';
 
 
 let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
 let subscription = button.addListener(async () => {
-	playTvShow();
+	playTvShow('Teen Titans Go!');
 });
 
-function playTvShow(){
+function playTvShow(tvshowTitle){
 	//First turn kodi activity on and start pinging it
 	tvOnOff();
 	pingKodi(()=>{
 		console.log('====> Kodi is alive!!')
-		buildPlaylist()
+		buildPlaylist(tvshowTitle)
 	});
-
-	
 }
 
-
-function buildPlaylist(){ console.log('build playlist')
+function buildPlaylist(tvshowTitle){
 
 	kodi(KODI_IP_ADDRESS, 9090).then(function (connection) {
-		return connection.VideoLibrary.GetEpisodes({ 
-			'tvshowid': 48, //teen titans go! 
-			'limits': { 
-				'start' : 0, 
-				'end': 10 }, 
-			'sort': { 
-				'order': 'ascending', 
-				'method': 'random'} 
-			})
-		.then(function(data) {
+		//First get tvshowid of lable
+		return connection.VideoLibrary.GetTVShows()
+			.then(function(data) {
+				let tvshow = filter(data.tvshows, function(item) { return item.label===tvshowTitle })
 
-			//console.log(data.episodes)
+				return tvshow[0].tvshowid;
 
-			return connection.Playlist.Clear({playlistid:1}).then(function(){
-				return Promise.all(data.episodes.map(function(episode) { console.log(episode)
-					return connection.Playlist.Add({playlistid:1, item:{episodeid:episode.episodeid}})
-					}))
-				.then(function(e){ 
-						return connection.Player.Open({item: { playlistid: 1 }})
-					/*return connection.Playlist.GetItems({playlistid:1}).then(function(items){
-						console.log(items)
-					})*/
+			}).then(function(id){ console.log(id)
+				return connection.VideoLibrary.GetEpisodes({ 
+					'tvshowid': 48, //teen titans go! 
+					'limits': { 
+						'start' : 0, 
+						'end': 10 }, 
+					'sort': { 
+						'order': 'ascending', 
+						'method': 'random'} 
+					})
+				.then(function(data) {
+
+					console.log(data.episodes)
+
+					return connection.Playlist.Clear({playlistid:1}).then(function(){
+						return Promise.all(data.episodes.map(function(episode) { console.log(episode)
+							return connection.Playlist.Add({playlistid:1, item:{episodeid:episode.episodeid}})
+							}))
+						.then(function(e){ 
+								return connection.Player.Open({item: { playlistid: 1 }})
+							/*return connection.Playlist.GetItems({playlistid:1}).then(function(items){
+								console.log(items)
+							})*/
+						})
+
+					// })
 				})
 
 			})
-		})
+
+		
 
 	}).catch(function(e) {
 		/* Handle errors */
@@ -75,31 +86,31 @@ function buildPlaylist(){ console.log('build playlist')
 
 function tvOnOff(){
 	harmony(HARMONY_IP_ADDRESS)
-  	.then(function(harmonyClient) {
-   		harmonyClient.isOff()
-	      .then(function(off) {
-	        if(off) {
-	          console.log('Currently off. Turning TV on.')
+		.then(function(harmonyClient) {
+			harmonyClient.isOff()
+				.then(function(off) {
+					if(off) {
+						console.log('Currently off. Turning TV on.')
 
-	          harmonyClient.getActivities()
-	            .then(function(activities) {
-	              activities.some(function(activity) {
-	                if(activity.label === 'Watch Kodi') {
-	                  var id = activity.id
-	                  harmonyClient.startActivity(id)
-	                  harmonyClient.end()
-	                  return true
-	                }
-	                return false
-	              })
-	            })
-	        } else {
-	          console.log('Currently on. Turning TV off')
-	          harmonyClient.turnOff()
-	          harmonyClient.end()
-	        }
-	      })
-  	})
+						harmonyClient.getActivities()
+							.then(function(activities) {
+								activities.some(function(activity) {
+									if(activity.label === 'Watch Kodi') {
+										var id = activity.id
+										harmonyClient.startActivity(id)
+										harmonyClient.end()
+										return true
+									}
+									return false
+								})
+							})
+					} else {
+						console.log('Currently on. Turning TV off')
+						harmonyClient.turnOff()
+						harmonyClient.end()
+					}
+				})
+		})
 }
 
 
@@ -109,7 +120,7 @@ function pingKodi(cb){
 			return console.log('====> Fail');
 		}
 		return cb();
-  })
+	})
 }
 
 
